a68a78d7174fde8e8d53be50e7dad437ad882c23
KhloudIbrahem <Khloudk130@gmail.com>
2023-06-17 20:16:24 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 in success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	putchar('\n');
+	return (0);
+}
