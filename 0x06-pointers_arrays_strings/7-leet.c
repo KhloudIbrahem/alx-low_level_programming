709e0e1f5824222d03e980bd0b5f4cf79f661313
KhloudIbrahem <Khloudk130@gmail.com>
2023-06-30 04:44:49 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337.
+ * @s: the array of characters.
+ * Return: The encoded string.
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char arr1[] = "aAeEoOtTlL";
+	char arr2[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == arr1[j])
+				s[i] = arr2[j];
+		}
+	}
+	return (s);
+}
