de6c4beca96dbba3091b732d704329edc24d7b1a
KhloudIbrahem <Khloudk130@gmail.com>
2023-07-04 16:40:21 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - Prints the character in the stdout.
+ * @c: the character to be printed.
+ * Return: 1 in success or -1 in error.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
