b0bd2641a17a90736c9e5242c3f67ec99d18640e
KhloudIbrahem <Khloudk130@gmail.com>
2023-07-04 17:51:44 -0700

    Task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: the buffer.
+ * @to: the character.
+ * Return: nothing.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
