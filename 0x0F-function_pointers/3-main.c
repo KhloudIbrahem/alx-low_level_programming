f29f949236359a27229f30fa2a0181d66a7f45fd
KhloudIbrahem <Khloudk130@gmail.com>
2023-07-19 15:46:53 -0700

    Task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+/**
+ * main - This function we have a two number and an operator
+ * @ac: Is the length of Number of arguments
+ * @av: These are the arguments
+ * Return: Return to 0
+ */
+int main(int argc, char *argv[])
+{
+	int numb1, numb2;
+	int (*fun)(int, int);
+	char *c = argv[2];
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((*c != '+' && *c != '-' && *c != '*' && *c != '/' && *c != '%') ||
+			*(c + 1) != 0)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	numb1 = atoi(argv[1]);
+	numb2 = atoi(argv[3]);
+	if ((*c == '/' || *c == '%') && numb2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	fun = get_op_func(c);
+	printf("%d\n", fun(numb1, numb2));
+	return (0);
+}
