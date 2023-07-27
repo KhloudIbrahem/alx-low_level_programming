40a9526efce756dd306f1265f6b970e15c13f7ed
KhloudIbrahem <Khloudk130@gmail.com>
2023-07-27 02:33:21 -0700

    Task 2 update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	while (str[size])
 		size++;
-	node = malloc(sizeof(list_t ));
+	node = malloc(sizeof(list_t));
 	if (node == NULL)
 		return (NULL);
 	node->str = strdup(str);
